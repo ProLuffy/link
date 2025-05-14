4370982f32eb0fbfec8d7af1ddb36de96702c86b
Pro Luffy <mmjoker786@gmail.com>
2025-05-14 13:58:11 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,3 +6,4 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+# +++ Made By Obito [telegram username: @i_killed_my_clan] +++ #
